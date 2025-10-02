cd01dfe9bb0535945c6e4ed6fb287d966804f149
pengqianhan <hanpengqian@outlook.com>
2025-10-02 14:41:43 +1300

    Update `global_max_sample_num` in `main_gemini.py` from 3 to 10 for testing purposes. Modify `run_llmsr.sh` to include execution command for `main_gemini.py` with the new API model configuration for `oscillator2`.

--- run_llmsr.sh
+++ run_llmsr.sh
@@ -48,4 +48,5 @@
 
 
 # python main.py --use_api True --api_model "gpt-3.5-turbo" --problem_name oscillator2 --spec_path ./specs/specification_oscillator2_torch.txt --log_path ./logs/oscillator2_gpt3.5_torch
-python main.py --problem_name oscillator2 --spec_path ./specs/specification_oscillator2_torch.txt --log_path ./logs/oscillator2_local_torch
+# python main.py --problem_name oscillator2 --spec_path ./specs/specification_oscillator2_torch.txt --log_path ./logs/oscillator2_local_torch
+python main_gemini.py --use_api True --api_model "models/gemini-flash-lite-latest" --problem_name oscillator2 --spec_path ./specs/specification_oscillator2_torch.txt --log_path ./logs/oscillator2_gemini_flash_lite
